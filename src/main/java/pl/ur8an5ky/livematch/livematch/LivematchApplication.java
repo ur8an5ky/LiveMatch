8a227429685ff882519d3ca24193194e8348c4f8
ur8an5ky <urbanski.jakub77@gmail.com>
2026-04-29 13:57:07 +0200

    feat: Spring Boot project skeleton from Spring Initializr

---
+++ src/main/java/pl/ur8an5ky/livematch/livematch/LivematchApplication.java
@@ -0,0 +1,13 @@
+package pl.ur8an5ky.livematch.livematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivematchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ur8an5ky/livematch/livematch/LivematchApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ur8an5ky.livematch.livematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
