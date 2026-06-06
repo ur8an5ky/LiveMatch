6f443986b0d006c2be4b67e034c9535907338fd8
ur8an5ky <urbanski.jakub77@gmail.com>
2026-06-06 16:18:59 +0200

    test(integration): document Spring context loading smoke test

--- src/test/java/pl/ur8an5ky/livematch/LivematchApplicationTests.java
+++ src/test/java/pl/ur8an5ky/livematch/LivematchApplicationTests.java
@@ -2,12 +2,15 @@ package pl.ur8an5ky.livematch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.jpa.hibernate.ddl-auto=validate"
+})
 class LivematchApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
